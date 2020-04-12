6d67c2927ce83296243956105e349a65b1628d04
samuelmorenov <UO266321@uniovi.es>
2020-04-12 17:41:11 +0200

    SDI-NodeJS-10.2- SW API REST Autenticación

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -55,31 +55,62 @@ module.exports = function (app, gestorBD) {
         });
     });
 
-    app.put("/api/cancion/:id", function(req, res) {
+    app.put("/api/cancion/:id", function (req, res) {
 
-        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
 
         let cancion = {}; // Solo los atributos a modificar
-        if ( req.body.nombre != null)
+        if (req.body.nombre != null)
             cancion.nombre = req.body.nombre;
-        if ( req.body.genero != null)
+        if (req.body.genero != null)
             cancion.genero = req.body.genero;
-        if ( req.body.precio != null)
+        if (req.body.precio != null)
             cancion.precio = req.body.precio;
-        gestorBD.modificarCancion(criterio, cancion, function(result) {
+        gestorBD.modificarCancion(criterio, cancion, function (result) {
             if (result == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(200);
                 res.json({
-                    mensaje : "canción modificada",
-                    _id : req.params.id
+                    mensaje: "canción modificada",
+                    _id: req.params.id
                 })
             }
         });
+    });
+
+    app.post("/api/autenticar/", function (req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let criterio = {
+            email: req.body.email,
+            password: seguro
+        }
+
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                res.status(401);
+                res.json({
+                    autenticado: false
+                })
+            } else {
+                var token = app.get('jwt').sign(
+                    {
+                        usuario: criterio.email,
+                        tiempo: Date.now() / 1000
+                    },
+                    "secreto");
+                res.status(200);
+                res.json(
+                    {
+                        autenticado: true,
+                        token: token
+                    })
+            }
+        })
     });
 };
 
