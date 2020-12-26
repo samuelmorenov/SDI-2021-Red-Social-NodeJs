447f63b1921a89f36bc888d194c60a43fa0e309e
samuelmorenov <xamelsamuel@gmail.com>
2020-12-26 18:34:26 +0100

    Añadidos nuevos campos en la funcion signup

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,19 +1,38 @@
 module.exports = function (app, swig, gestorBD) {
 
     app.post('/signup', function (req, res) {
-        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        let usuario = {
-            email: req.body.email,
-            password: seguro
+
+        if (req.body.password != req.body.passwordConfirm) {
+            res.redirect("/registrarse?mensaje=Las contraseñas no coinciden");
+        } else {
+            let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+
+            let criterio = {
+                email: req.body.email
+            };
+
+            gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+                if (usuarios.length > 0) {
+                    res.redirect("/signup?mensaje=Email ya registrado en el sistema");
+                } else {
+                    let usuario = {
+                        nombre: req.body.nombre,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        password: seguro
+                    };
+
+                    gestorBD.insertarUsuario(usuario, function (id) {
+                        if (id == null) {
+                            res.redirect("/signup?mensaje=Error al registar usuario");
+                        } else {
+                            res.redirect("/login?mensaje=Nuevo usuario registrado")
+                        }
+                    });
+                }
+            });
         }
-        gestorBD.insertarUsuario(usuario, function (id) {
-            if (id == null) {
-                res.redirect("/signup?mensaje=Error al registar usuario");
-            } else {
-                res.redirect("/login?mensaje=Nuevo usuario registrado")
-            }
-        });
     });
 
     app.get("/signup", function (req, res) {
