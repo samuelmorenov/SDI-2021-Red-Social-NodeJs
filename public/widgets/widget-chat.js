b3c9fb3000f957bd85fb60988bae22ffa5205b40
samuelmorenov <xamelsamuel@gmail.com>
2021-01-09 18:44:18 +0100

    Añadido boton a chat con amigos

---
+++ public/widgets/widget-chat.js
@@ -0,0 +1,1 @@
+window.history.pushState("", "", "/cliente.html?w=chat/");
--- public/widgets/widget-friends.js
+++ public/widgets/widget-friends.js
@@ -8,27 +8,33 @@ function loadFriends() {
     $.ajax({
         url: URLbase + "/friends",
         type: "GET",
-        data: { },
+        data: {},
         dataType: 'json',
-        headers: { "token": token },
-        success: function(respuesta) {
+        headers: {"token": token},
+        success: function (respuesta) {
             friends = respuesta;
             friendsTable(friends);
         },
-        error : function (error) {
-            $( "#contenedor-principal" ).load("widgets/" + "widget-login.html");
+        error: function (error) {
+            $("#contenedor-principal").load("widgets/" + "widget-login.html");
         }
     });
 }
 
 function friendsTable(friends) {
-    $( "#tbody-friends" ).empty();
+    $("#tbody-friends").empty();
     for (let i = 0; i < friends.length; i++) {
-        $( "#tbody-friends" ).append(
-            "<tr id="+friends[i].email+">"+
-            "<td>"+friends[i].name+"</td>" +
-            "<td>"+friends[i].lastName+"</td>" +
-            "<td>"+friends[i].email+"</td>" +
+        $("#tbody-friends").append(
+            "<tr id=" + friends[i].email + ">" +
+            "<td>" + friends[i].name + "</td>" +
+            "<td>" + friends[i].lastName + "</td>" +
+            "<td>" + friends[i].email + "</td>" +
+            "<td>" +
+                "<button type='submit' className='btn btn-default' " +
+                "id='button-chat-" + friends[i].email + "' "+
+                "onclick=chat('" + friends[i].email + "')" +
+                ">" + "✉</button>" +
+            "</td>" +
             "</tr>"
         );
     }
@@ -39,9 +45,13 @@ $('#button-buscar').click(function () {
     var inputName = $("#input-buscar").val();
 
     for (i = 0; i < friends.length; i++) {
-        if(friends[i].name.includes(inputName)){
+        if (friends[i].name.includes(inputName)) {
             friendsFiltereds.push(friends[i]);
         }
     }
     friendsTable(friendsFiltereds);
 });
+
+function chat(friendEmail) {
+    $("#contenedor-principal").load("widgets/widget-chat.html");
+}
