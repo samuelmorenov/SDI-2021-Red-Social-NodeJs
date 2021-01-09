5335fe926106c7ec95cf05852e3b015b2f1c511b
samuelmorenov <xamelsamuel@gmail.com>
2021-01-09 01:32:31 +0100

    Añadido widget y route de friends (No funciona)

--- public/widgets/widget-friends.js
+++ public/widgets/widget-friends.js
@@ -1,1 +1,36 @@
 window.history.pushState("", "", "/cliente.html?w=friends");
+
+this.loadFriends();
+
+function loadFriends() {
+    //var token = Cookies.get('token');
+
+    $.ajax({
+        url: URLbase + "/friends",
+        type: "GET",
+        data: { },
+        dataType: 'json',
+        headers: { "token": token },
+        success: function(respuesta) {
+            friends = respuesta;
+            friendsTable(friends);
+        },
+        error : function (error) {
+            $( "#contenedor-principal" ).load("widgets/" + "widget-login.html");
+        }
+    });
+}
+
+function friendsTable(friends) {
+    $( "#tbody-friends" ).empty();
+    for (let i = 0; i < friends.length; i++) {
+        $( "#tbody-friends" ).append(
+            "<tr id="+friends[i].email+">"+
+            "<td>"+friends[i].name+"</td>" +
+            "<td>"+friends[i].lastName+"</td>" +
+            "<td>"+friends[i].email+"</td>" +
+            "</tr>"
+        );
+    }
+}
+
---
+++ routes/serviciosWeb/rFriends.js
@@ -0,0 +1,49 @@
+module.exports = function (app, gestorBD) {
+
+    app.get("/api/friends/", function(req, res) {
+
+        let userEmail = res.usuario;
+
+        console.log("userEmail: "+userEmail);
+        let criterio = {
+            $or:
+                [
+                    {usuario1Email: userEmail},
+                    {usuario2Email: userEmail},
+                ]
+        };
+
+        gestorBD.obtenerLista('amistades', criterio, function (amistades) {
+
+            if (amistades == null) {
+                res.status(500);
+                res.json({
+                    error: "Error: No se ha podido obtener la lista de amistades"
+                });
+            } else {
+                let amigos = [];
+                for (let i = 0; i < amistades.length; i++) {
+                    if (amistades[i].usuario1Email != userEmail) {
+                        amigos.push(amistades[i].usuario1Email);
+                    } else if (amistades[i].usuario2Email != userEmail) {
+                        amigos.push(amistades[i].usuario2Email);
+                    }
+                }
+                criterio = {email: {$in: amigos}};
+                gestorBD.obtenerLista('usuarios', criterio, function (usuarios, total) {
+
+                    if (usuarios == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Error: No se ha podido obtener la lista de amistades"
+                        });
+                    } else {
+                        res.status(200);
+                        res.send(JSON.stringify(usuarios));
+                    }
+                });
+            }
+        });
+    });
+
+}
